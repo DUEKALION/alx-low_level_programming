1a9f6466abf0e9536ef0054f594bb778440112d6
AyBamBam <aycodergymer21@gmail.com>
2022-09-11 17:38:40 -0700

    Added advanced tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,13 @@
 /**
  * main - Prints size of characters
  *
- * returns: returns 0 always
+ * Return: returns 0 always
  */
 
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+i	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
