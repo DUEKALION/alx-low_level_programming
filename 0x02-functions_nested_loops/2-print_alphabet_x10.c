a26ee3976e37f1cfb53e4c7c7308ea2d0755c8cb
AyBamBam <aycodergymer21@gmail.com>
2022-09-14 18:56:25 -0700

    Added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check description
+ * Description: print 10x all alphabets in lower case
+ * Return: Null.
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		for(x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
