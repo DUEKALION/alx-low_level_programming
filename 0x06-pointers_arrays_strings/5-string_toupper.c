68042cd87cb6e2a7e5c6c4029fd3abac3164fc7e
AyBamBam <aycodergymer21@gmail.com>
2022-09-22 10:49:44 -0700

    Added file 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - characters to uppercase
+ * @s: input string
+ * Return: pointer to string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
