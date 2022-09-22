34b29eba76160a9a0f815666be2e7d8538c7bd52
AyBamBam <aycodergymer21@gmail.com>
2022-09-22 11:36:09 -0700

    Added file 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encode a string to leet
+ * @s: input string
+ * Return: pointer s
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char b[] = "ol_ea__t;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; b[j] != '\0'; j++)
+		{
+			if (s[i] == b[j] || s[i] == (b[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+	return (s);
+}
