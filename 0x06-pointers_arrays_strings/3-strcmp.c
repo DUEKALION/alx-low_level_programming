28666485bc3b3d031020e414523ab148aad14acd
AyBamBam <aycodergymer21@gmail.com>
2022-09-22 12:46:04 -0700

    Fixed 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
+	while ((*s1 != '\n' && *s2 != '\0') && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++, s2++;
+		s1++;
+		s2++;
 	}
-	return (*s1 - *s2);
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 - *s2);
+}
